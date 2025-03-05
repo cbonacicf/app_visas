62e37d9e31e0b53931ed7f7be07a398c77b29ca4
Claudio Bonacic <cbonacic@Claudios-iMac.local>
2025-03-05 18:05:46 -0300

    Retorno a conexión interna

--- app.py
+++ app.py
@@ -33,22 +33,15 @@ from sqlalchemy.ext.automap import automap_base
 
 #objeto_url = URL.create(
 #    'postgresql+psycopg2',
-#    username = 'postgres',
-#    host = 'localhost',
-#    database = 'gestion_visas',
+#    username = os.environ['PGUSER'],
+#    password = os.environ['PGPASSWORD'],
+#    host = os.environ['PGHOST'],
+#    port = os.environ['PGPORT'],
+#    database = os.environ['PGDATABASE'],
 #)
 
-objeto_url = URL.create(
-    'postgresql+psycopg2',
-    username = os.environ['PGUSER'],
-    password = os.environ['PGPASSWORD'],
-    host = os.environ['PGHOST'],
-    port = os.environ['PGPORT'],
-    database = os.environ['PGDATABASE'],
-)
-
-engine = create_engine(objeto_url)
-#engine = create_engine(os.environ['DATABASE_URL'])
+#engine = create_engine(objeto_url)
+engine = create_engine(os.environ['DATABASE_URL'])
 
 ### Fuentes externas
 Conversion = namedtuple('Conversion', ['app', 'ex', 'mig', 'lec', 'mod'])
